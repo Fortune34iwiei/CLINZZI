267ec1b55341027e8e1f37db9eb0f88e220b0725
Fortune34iwiei <144454188+Fortune34iwiei@users.noreply.github.com>
2026-01-26 10:42:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-export const CONFIG = {
-  BOT_TOKEN: "YOUR_BOT_TOKEN",
-  CHAT_ID: "YOUR_CHAT_ID"
-};
+// Keep this external for security
+export const TELEGRAM_TOKEN = "YOUR_BOT_TOKEN";
+export const TELEGRAM_CHAT_ID = "YOUR_CHAT_ID";
