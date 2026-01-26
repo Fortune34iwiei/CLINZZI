f7a5b7d3692dc8b55e17f0709396f15d8262927b
Fortune34iwiei <144454188+Fortune34iwiei@users.noreply.github.com>
2026-01-26 10:43:46 +0100

    Update Telegram bot token and chat ID
    Updated Telegram bot token and chat ID in config.js.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Keep this external for security
-export const TELEGRAM_TOKEN = "YOUR_BOT_TOKEN";
-export const TELEGRAM_CHAT_ID = "YOUR_CHAT_ID";
+export const TELEGRAM_TOKEN = "7781718948:AAF90lWhR0vfih40-foHBSDjGTcB4HRC64A";
+export const TELEGRAM_CHAT_ID = "7829354744";
